20d9acf5dbb6a6d214377404e8a93d06e6ae5870
El-basel <abdelrahman.elbasel42@gmail.com>
2025-02-01 19:49:46 +0200

    Update page.tsx

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -4,12 +4,6 @@ const HomePage = () => {
   return (
     <>
       <Net />
-      <main className="flex flex-col items-center justify-center h-screen">
-        <h1 className="mb-4 select-none text-4xl font-bold text-center text-red-800 md:text-6xl lg:text-8xl neonText fade-in">
-          Abdelrahman Elbasel
-        </h1>
-        <HomeNav />
-      </main>
     </>
   );
 };
