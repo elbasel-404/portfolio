d92521ad59b6fcf5fad90bd9703ac3796c3fab79
elbasel <abdelrahman.elbasel42@gmail.com>
2026-03-11 23:16:34 +0200

    remove download button

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -14,13 +14,6 @@ const AboutPage = () => {
   return (
     <SlideInFromBottom>
       <HomeButton />
-      <Link
-        className="fixed z-20 flex flex-col gap-2 px-2 py-2 border rounded-lg hover:scale-125 bottom-4 border-white/50 left-4"
-        href="/Abdelrahman-Elbasel-Resume.pdf"
-        download="Abdelrahman-Elbasel-Resume.pdf"
-      >
-        <FaDownload className="w-6 h-6 active:scale-125 hover:bounce" />
-      </Link>
       <Dots />
       <main className="h-[100dvh] overflow-y-auto snap-y scroll-smooth snap-mandatory app-scrollbar">
         <AboutPageSection className="items-center justify-center">
