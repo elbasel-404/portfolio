57a0ccd9f939797042477e8a3e8c4326888b438a
elbasel42 <abdelrahman.elbasel42@gmail.com>
2026-09-18 19:57:59 +0300

    optimize script loading

--- app/components/SlideInFromBottom.tsx
+++ app/components/SlideInFromBottom.tsx
@@ -22,9 +22,9 @@ export const SlideInFromBottom = ({
     <div className="h-[100dvh] overflow-hidden">
       <div
         className={twMerge(
-          "transition-transform h-[100dvh] duration-3000 translate-y-1000",
+          "transition-transform h-[100dvh] duration-1000 translate-y-1000",
           className,
-          transition
+          transition,
         )}
       >
         {children}
